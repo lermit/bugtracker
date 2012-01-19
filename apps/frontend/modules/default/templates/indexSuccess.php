82a33ecc0fe76a944cab80037b6b4bd694e467f4
Romain THERRAT <romain42@gmail.com>
2012-01-19 20:43:35 +0100

    ADD: Link on image

--- apps/frontend/modules/default/templates/indexSuccess.php
+++ apps/frontend/modules/default/templates/indexSuccess.php
@@ -14,18 +14,18 @@
 <div class="row">
   <div class="span4">
     <h2>Create an account</h2>
-    <img src="/images/create-account.png" alt="Create an account" Title="Create an account" />
+    <a href="#" title="Create an account"><img src="/images/create-account.png" alt="Create an account" Title="Create an account" /></a>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at sem magna. Donec sed nunc non tortor aliquam consequat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec a purus tellus, nec hendrerit lacus. Aliquam erat volutpat. Vestibulum tristique nisi et lacus porttitor imperdiet aliquet arcu consectetur. Integer id fermentum enim. Nam elit orci, dapibus commodo egestas ut, rhoncus feugiat arcu. Maecenas euismod convallis ultrices. Sed at pharetra leo. Integer nec nibh odio, non facilisis tortor. In pharetra tellus eget metus condimentum volutpat.</p>
 
   </div>
   <div class="span4 offset2">
     <h2>Fill a ticket</h2>
-    <img src="/images/create-ticket.png" alt="Create a ticket" Title="Create a ticket" />
+    <a href="<?php echo url_for("@ticket_add") ?>"><img src="/images/create-ticket.png" alt="Create a ticket" Title="Create a ticket" /></a>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at sem magna. Donec sed nunc non tortor aliquam consequat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec a purus tellus, nec hendrerit lacus. Aliquam erat volutpat. Vestibulum tristique nisi et lacus porttitor imperdiet aliquet arcu consectetur. Integer id fermentum enim. Nam elit orci, dapibus commodo egestas ut, rhoncus feugiat arcu. Maecenas euismod convallis ultrices. Sed at pharetra leo. Integer nec nibh odio, non facilisis tortor. In pharetra tellus eget metus condimentum volutpat.</p>
   </div>
   <div class="span4 offset2">
     <h2>Manage Account</h2>
-    <img src="/images/configure-dashboard.png" alt="Configure your dashboard" Title="Configure your dashboard" />
+    <a href="#" title="Configure your dashboard"><img src="/images/configure-dashboard.png" alt="Configure your dashboard" Title="Configure your dashboard" /></a>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas at sem magna. Donec sed nunc non tortor aliquam consequat. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec a purus tellus, nec hendrerit lacus. Aliquam erat volutpat. Vestibulum tristique nisi et lacus porttitor imperdiet aliquet arcu consectetur. Integer id fermentum enim. Nam elit orci, dapibus commodo egestas ut, rhoncus feugiat arcu. Maecenas euismod convallis ultrices. Sed at pharetra leo. Integer nec nibh odio, non facilisis tortor. In pharetra tellus eget metus condimentum volutpat.</p>
   </div>
 </div>
